1d6e232272a668ad206e3c1119e717d6e39b4fdd
danmarq <danilo_marques@outlook.com>
2024-03-16 17:16:48 -0300

    movendo logo pra cima

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -38,6 +38,9 @@ export default function Popup () {
                         <Image className="img-fluid" src={'/images/phone_app_screen.png'} width={500} height={457} alt=""></Image>
                     </div>
                     <div className="popup-content-two">
+                        <div className="popup-logo">
+                            <Image className="img-fluid" src={'/images/sendwave_logo.png'} width={130} height={13} alt=""></Image>
+                        </div>
                         <Image className="img-fluid letters" src={'/images/letteres.png'} width={300} height={249} alt=""></Image>
                         <div className="store-buttons">
                             <a href="https://apps.apple.com/us/app/sendwave-send-money/id846717081" onClick={fecharModal} className="" target="_blank">
@@ -47,9 +50,6 @@ export default function Popup () {
                             <a href="https://play.google.com/store/apps/details?id=com.mychime.waveremit.app" onClick={fecharModal} className="" target="_blank">
                                 <Image className="img-fluid" src={'/images/playStoreButton.png'} width={200} height={49} alt=""></Image>
                             </a>
-                        </div>
-                        <div className="popup-logo">
-                            <Image className="img-fluid" src={'/images/sendwave_logo.png'} width={100} height={13} alt=""></Image>
                         </div>
                     </div>
                 </div>
